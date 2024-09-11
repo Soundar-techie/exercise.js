44f1b3cf14fb876812ebcd434a50249c3a0aa04b
soundar <153784402+Soundar-techie@users.noreply.github.com>
2024-09-11 20:02:46 +0530

    Update object-method.js

--- object-method.js
+++ object-method.js
@@ -2,8 +2,9 @@ const person = {
     firstName: "leo",
     lastName: "das",
     id: 5566,
-    fullName: function() {
-      return this.firstName + " " + this.lastName;
-    }
-  };
-  console.log(person.fullName())
+};
+
+   person.name = function() {
+  return this.firstName + " " + this.lastName;
+}
+console.log (person.name())
