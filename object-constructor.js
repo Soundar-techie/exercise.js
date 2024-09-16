d687d7982377375f0e130e4a45f3750b1eecc4f5
soundar <vksoundar2020@gmail.com>
2024-09-16 20:42:54 +0530

    id

---
+++ object-constructor.js
@@ -0,0 +1,9 @@
+//constructor function in object
+function Id(name,place,bloodgroup,phonenumber){
+    this.name = name;
+    this.place = place;
+    this.bg = bloodgroup;
+    this.phone = phonenumber;
+}
+//strong the another variable
+const myid = new Id("bala","selam","o+",9876453413)
