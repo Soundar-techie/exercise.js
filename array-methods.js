cf6579d021e611668b7512ad4c7d869fc6f0c77d
soundar <vksoundar2020@gmail.com>
2024-10-14 18:43:59 +0530

    array methods

---
+++ array-methods.js
@@ -0,0 +1,16 @@
+const namelist1 =["arul","kumar","pradeep"];
+namelist1.pop();
+
+const namelist2 =["arul","kumar","pradeep"];
+namelist2.push("jeeva")
+
+const namelist3 =["arul","kumar","pradeep"];
+namelist3.shift()
+
+const namelist4 =["arul","kumar","pradeep"];
+namelist4.unshift("ravi")
+
+const namelist5 =["arul","kumar","pradeep"];
+delete namelist5[2];//Use pop() or shift() instead
+
+//concat() using add to arrays
