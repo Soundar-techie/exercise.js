3b19d78d396cfc21b3cc308b780307b030f9f023
soundar <vksoundar2020@gmail.com>
2024-11-15 19:04:04 +0530

    age loop

---
+++ simple-test/do-while.js
@@ -0,0 +1,7 @@
+let age = 10;
+
+do{
+   console.log("YOUR AGE IS" + " " +age) 
+    age++;
+}
+while(age < 20 )
---
+++ simple-test/while-loop.js
@@ -0,0 +1,5 @@
+let age = 10;
+while(age < 20 ){
+    console.log("MY AGE IS" + " " +age) 
+    age++;
+}
